54a474d386ebf5db862af72abb65f00e7f47196b
Robby Bourne <robbybourne1@gmail.com>
2017-09-24 12:11:37 -0400

    F# fixed string stuff

--- Program.fs
+++ Program.fs
@@ -12,26 +12,19 @@ let add x y =
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    let x = Console.ReadLine()
-    let y = Console.ReadLine()
+    let x = Convert.ToInt32(Console.ReadLine())
+    let y = Convert.ToInt32(Console.ReadLine())
     let sum = add x y
     Console.WriteLine sum
     0 // return an integer exit code
 
 //  C# code
-//  using System;
-// using System.Collections.Generic;
-// using System.IO;
-// class Solution {
-//     static int solveMeFirst(int a, int b) { 
-//       return a + b;  
-      
-//     }
+//     
 //     static void Main(String[] args) {
 //         int val1 = Convert.ToInt32(Console.ReadLine());
 //         int val2 = Convert.ToInt32(Console.ReadLine());
 //         int sum = solveMeFirst(val1,val2);
 //         Console.WriteLine(sum);
 //     }
-// }  
+//   
 
